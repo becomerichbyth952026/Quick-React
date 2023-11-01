1557ac958b8972622860d63ff5c5dfd9c010ad93
becomerichbythe952026 <nl91alessandro@gmail.com>
2023-11-01 18:46:01 +0100

    task2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App';
+import './logo.svg';
+import './index.js'
+import { BrowserRouter, Routes, Route,} from "react-router-dom";
+import { GameInfo } from './components/GameInfo.jsx';
+import { ScheduleGame } from './components/ScheduleGame';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<GameInfo/>}></Route>
+      <Route path="/other" element={<ScheduleGame/>}></Route>
+    </Routes>
+    </BrowserRouter>
+
+      
     </div>
   );
 }
---
+++ my-app/src/components/GameInfo.jsx
@@ -0,0 +1,153 @@
+import React from "react"
+
+
+
+export function GameInfo() {
+  return (
+    <div class="main_table"> 
+
+      <div class="title-bar"><h2>NYSL Game Information</h2></div>
+      
+      <div class="upper_title">
+      <h3>Fall Schedule</h3>
+      <p class="saturday"><b><u>* All games take place on Saturday</u></b></p>
+      </div>
+
+      <table class="table ">
+        <thead>
+          <tr>
+            <th scope="col">SEPTEMBER</th>
+            <th scope="col">Teams</th>
+            <th scope="col">Location</th>
+            <th scope="col">Times</th>
+          </tr>
+        </thead>  
+
+        <tbody>
+          <tr scope="row">
+            <td>9/01</td>
+            <td>U1 and U4</td>
+            <td>AJ Katzenmaier</td>
+            <td>9:30 a.m.</td>
+          </tr>
+          <tr scope="row">
+            <td></td>
+            <td>U3 and U2</td>
+            <td>Greenbay</td>
+            <td>1:00 p.m.</td>
+          </tr>
+          <tr>
+            <td scope="row">9/08</td>
+            <td>U5 and U6</td>
+            <td>Howard A Yeager</td>
+            <td>9:30 a.m.</td>
+          </tr>
+          <tr>
+            <td scope="row"></td>
+            <td>U6 and U1</td>
+            <td>Marjorie P Hart</td>
+            <td>1:00 p.m.</td>
+          </tr>
+          <tr>
+            <td scope="row">9/15</td>
+            <td>U2 and U4</td>
+            <td>North</td>
+            <td>9:30 a.m.</td>
+          </tr>
+          <tr>
+            <td scope="row"></td>
+            <td>U3 and U5</td>
+            <td>AJ Katzenmaier</td>
+            <td>1:00 p.m.</td>
+          </tr>
+          <tr>
+            <td scope="row">9/22</td>
+            <td>U1 and U3</td>
+            <td>South</td>
+            <td>9:30 a.m.</td>
+          </tr>
+          <tr>
+            <td scope="row"></td>
+            <td>U2 and U6</td>
+            <td>Howard A Yeager</td>
+            <td>1:00 p.m.</td>
+          </tr>
+          <tr>
+            <td scope="row">9/29</td>
+            <td>U4 and U5</td>
+            <td>Greenbay</td>
+            <td>9:30 a.m.</td>
+          </tr>
+        </tbody>
+      
+
+      
+      
+        <thead>
+          <tr>
+            <th scope="col">OCTOBER</th>
+            <th scope="col">Teams</th>
+            <th scope="col">Location</th>
+            <th scope="col">Times</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>10/06</td>
+            <td>U2 and U5</td>
+            <td>Marjorie P Hart</td>
+            <td>9:30 a.m.</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>U1 and U6</td>
+            <td>South</td>
+            <td>1:00 p.m.</td>
+          </tr>
+          <tr>
+            <td>10/13</td>
+            <td>U3 and U4</td>
+            <td>Howard A Yeager</td>
+            <td>9:30 a.m.</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>U5 and U1</td>
+            <td>Greenbay</td>
+            <td>1:00 p.m.</td>
+          </tr>
+          <tr>
+            <td>10/20</td>
+            <td>U6 and U3</td>
+            <td>North</td>
+            <td>9:30 a.m.</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>U2 and U4</td>
+            <td>Marjorie P Hart</td>
+            <td>1:00 p.m.</td>
+          </tr>
+          <tr>
+            <td>10/27</td>
+            <td>U3 and U1</td>
+            <td>AJ Katzenmaier</td>
+            <td>9:30 a.m.</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>U5 and U6</td>
+            <td>Howard A Yeager</td>
+            <td>1:00 p.m.</td>
+          </tr>
+        </tbody>
+      </table>
+    
+
+    </div>
+  );
+}
+
+
+
+
